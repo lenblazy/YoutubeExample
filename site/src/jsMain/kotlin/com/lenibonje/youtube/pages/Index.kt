f43a47e7e6946780fb053fbe7429976db822e09d
lenblazy <lenblazy@gmail.com>
2023-10-27 07:33:57 +0300

    Add Input field

--- site/src/jsMain/kotlin/com/lenibonje/youtube/pages/Index.kt
+++ site/src/jsMain/kotlin/com/lenibonje/youtube/pages/Index.kt
@@ -1,6 +1,6 @@
 package com.lenibonje.youtube.pages
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
@@ -9,12 +9,16 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fontSize
 import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.icons.fa.FaFaceSmile
 import com.varabyte.kobweb.silk.components.icons.fa.IconSize
 import com.varabyte.kobweb.silk.components.icons.fa.IconStyle
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.attributes.placeholder
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Input
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
@@ -26,6 +30,7 @@ fun HomePage() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var name by remember { mutableStateOf("") }
         Row(verticalAlignment = Alignment.CenterVertically) {
             FaFaceSmile(
                 modifier = Modifier.margin(right = 10.px),
@@ -35,10 +40,20 @@ fun HomePage() {
 
             P(
                 attrs = Modifier.fontSize(40.px).toAttrs()
-            ){
-                Text(value ="Hello, world")
+            ) {
+                Text(value = "Hello, $name")
             }
         }
+
+        Input(
+            type = InputType.Text,
+            attrs = Modifier.padding(topBottom = 10.px, leftRight = 20.px)
+                .fontSize(20.px)
+                .toAttrs {
+                    placeholder("Enter name")
+                    onInput { name = it.value }
+                }
+        )
     }
 
 
