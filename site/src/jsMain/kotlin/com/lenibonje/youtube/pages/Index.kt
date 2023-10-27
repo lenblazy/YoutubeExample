5190052ddbc957b1b1a4535f72c1ca7656368699
lenblazy <lenblazy@gmail.com>
2023-10-27 07:41:41 +0300

    Add custom theme for input field

--- site/src/jsMain/kotlin/com/lenibonje/youtube/pages/Index.kt
+++ site/src/jsMain/kotlin/com/lenibonje/youtube/pages/Index.kt
@@ -6,18 +6,22 @@ import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.fontSize
-import com.varabyte.kobweb.compose.ui.modifiers.margin
-import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.icons.fa.FaFaceSmile
 import com.varabyte.kobweb.silk.components.icons.fa.IconSize
 import com.varabyte.kobweb.silk.components.icons.fa.IconStyle
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
+import com.varabyte.kobweb.silk.components.style.focus
+import com.varabyte.kobweb.silk.components.style.toModifier
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.attributes.placeholder
+import org.jetbrains.compose.web.css.CSSColorValue
+import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.rgb
 import org.jetbrains.compose.web.dom.Input
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
@@ -47,7 +51,14 @@ fun HomePage() {
 
         Input(
             type = InputType.Text,
-            attrs = Modifier.padding(topBottom = 10.px, leftRight = 20.px)
+            attrs = InputStyle.toModifier()
+                .padding(topBottom = 10.px, leftRight = 20.px)
+                .outline(
+                    width = 0.px,
+                    style = LineStyle.None,
+                    color = Colors.Transparent
+                )
+                .backgroundColor(Theme.Gray.color)
                 .fontSize(20.px)
                 .toAttrs {
                     placeholder("Enter name")
@@ -57,4 +68,27 @@ fun HomePage() {
     }
 
 
+}
+
+val InputStyle by ComponentStyle {
+    base {
+        Modifier.border(
+            width = 2.px,
+            style = LineStyle.Solid,
+            color = Theme.Gray.color
+        )
+    }
+
+    focus {
+        Modifier.border(
+            width = 2.px,
+            style = LineStyle.Solid,
+            color = Theme.Blue.color
+        )
+    }
+}
+
+enum class Theme(val color: CSSColorValue) {
+    Gray(color = rgb(r = 238, g = 238, b = 238)),
+    Blue(color = rgb(r = 28, g = 181, b = 224)),
 }
