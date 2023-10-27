e10d89b36071d0cadcea367be4d8abb2e2677409
lenblazy <lenblazy@gmail.com>
2023-10-27 07:23:23 +0300

    create hello world website

--- site/src/jsMain/kotlin/com/lenibonje/youtube/pages/Index.kt
+++ site/src/jsMain/kotlin/com/lenibonje/youtube/pages/Index.kt
@@ -1,18 +1,45 @@
 package com.lenibonje.youtube.pages
 
-import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.icons.fa.FaFaceSmile
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.icons.fa.IconStyle
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            FaFaceSmile(
+                modifier = Modifier.margin(right = 10.px),
+                style = IconStyle.OUTLINE,
+                size = IconSize.XXL
+            )
+
+            P(
+                attrs = Modifier.fontSize(40.px).toAttrs()
+            ){
+                Text(value ="Hello, world")
+            }
+        }
     }
+
+
 }
